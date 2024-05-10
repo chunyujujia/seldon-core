21f11fac28cae84a36004d09a0b46b573d8dfc90
Lin Yiming <roymatrix.lin@gmail.com>
2024-05-10 11:35:59 +0800

    Revert "fix(operator): subscriptions to scheduler stop if scheduler stops or restarts. Further events, like model/server scaling, from scheduler will be ignored. To solve it, SeldonRuntimeController reconnects and subscribes to scheduler if its status is ready"
    This reverts commit 88654f67e7c0ca257924504b785f4326a55c86cb.

--- operator/controllers/mlops/seldonruntime_controller.go
+++ operator/controllers/mlops/seldonruntime_controller.go
@@ -169,12 +169,6 @@ func (r *SeldonRuntimeReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		return reconcile.Result{}, err
 	}
 
-	if seldonRuntime.Status.IsConditionReady(mlopsv1alpha1.SchedulerReady) {
-		if err := r.Scheduler.KeepConnection(seldonRuntime.Namespace); err != nil {
-			return reconcile.Result{}, err
-		}
-	}
-
 	return ctrl.Result{}, nil
 }
 
